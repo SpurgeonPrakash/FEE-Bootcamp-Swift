f4751cec1cccb183091ade7291a5618b42ec373c
Sai Siva Teja  Reddy Mallidi <saisivatejarm@virtusa.com>
2018-06-05 09:26:07 +0530

    images, styles,undo,reset modified

--- tictactoe/js/tictactoe.js
+++ tictactoe/js/tictactoe.js
@@ -1,5 +1,6 @@
 var players=[];
-var values=["X","O"];
+var values=['x','o'];
+undo_count=0;
 var scores=[0,0];
 players[0]="player1";
 players[1]="player2";
@@ -9,6 +10,8 @@ var turn=0;
 var store=0;
 var arr=new Array(9);
 var grid_no=0;
+var modal = document.getElementById('myModal');
+//var btn = document.getElementById("myBtn");
 function play(clickedBut,clickval)
 {
     grid_no=0;
@@ -25,7 +28,11 @@ function play(clickedBut,clickval)
             if(! gameover)
             {
                 scores[turn]+=store;
-                clickedBut.innerHTML="<span>"+ values[turn] +"</span>";
+
+		
+      		
+                clickedBut.style="background-image:url("+"images/"+values[turn] +".png)";
+
                 win();
                 if(! gameover)
                     switchplayers(); 
@@ -41,27 +48,53 @@ function switchplayers()
     document.getElementById("Tic-Tac-Toe").innerText= players[turn] + "'s turn";
 }
 function win(){
+
+		
     for(var i=0;i<winvalues.length;i++)
     {
         if((scores[turn] & winvalues[i])==winvalues[i]){
-            document.getElementById("Tic-Tac-Toe").innerText=players[turn] + "Wins!";
+		var playerText;
+
+		if(players[turn]=="player1"){
+		  playerText="Player 1";
+		}
+		else{
+		 playerText="Player 2";
+		}
+		
+   		modal.style.display = "block";
+
+		document.getElementById('msg').innerHTML=playerText+ "Wins!";
+		
+            //document.getElementById("Tic-Tac-Toe").innerText=players[turn] + "Wins!";
             gameover=true;
         }
     }
     if(((scores[0] + scores[1]) == 511) && !gameover){
+
+	modal.style.display = "block";
+	
+	
+	document.getElementById('msg').innerHTML="Game Over";
+	
         document.getElementById("Tic-Tac-Toe").innerText="TIE!!!"
         gameover=true;
     }
 }
 function undo()
 {
-    if(!gameover)
+    if(!gameover && undo_count<1)
     {
+        undo_count++;
         switchplayers();
         scores[turn]-=store;
         butpress.innerHTML="";
         arr[grid_no]=undefined;
     }
+    else if(!gameover)
+        alert("UNDO CAN BE DONE ONE TIME");
+    else
+        alert("UNDO CANNOT BE DONE AFTER WIN");
 }
 function reset()
 {
@@ -72,8 +105,24 @@ function reset()
     var buttons=document.getElementsByClassName("btn-group");
     for(var i=0;i<9;i++)
     {
-            buttons[i].innerHTML=" ";
+        buttons[i].style="background-image:url('')";
             arr[i]=undefined;
     }
+ modal.style.display = "none";
 }
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+
+
+
 
