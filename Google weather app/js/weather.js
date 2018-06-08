ef52b55647d46498d4e89b23f194620d5f5536d4
Kasa Lalith Kumar <lalithkumark@virtusa.com>
2018-06-08 19:25:58 +0530

    updated ui and added functionality

--- Google weather app/js/weather.js
+++ Google weather app/js/weather.js
@@ -1,4 +1,25 @@
 $('#search').click(()=>{
-    const place=$('#cname').val();
-    $('#searched-place').html(place);
+    const cityname=$('#cname').val();
+   // $('#searched-place').html(cityname);
+    function getWeatherDemo() {
+        $.get('https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast ' +
+              'where woeid in (select woeid from geo.places(1) where text="'+cityname+'")&format=json', function (data) {
+            console.log(data);
+            const weather_obj=data.query.results.channel;
+            const region=weather_obj.location.region;
+            const country=weather_obj.location.country;
+            const cname=weather_obj.location.city;
+            const condition=weather_obj.item.condition.text;
+            const ftemp=weather_obj.item.condition.temp;
+            const ctemp=changeFahtoCelsius(ftemp);
+            $('#searched-place').html(cname+','+region+','+country);
+            $('#cond').html(condition);
+            $('#temp').text(ctemp);  
+        });
+    }
+    getWeatherDemo();
 });
+function changeFahtoCelsius(fahrenheit)
+{
+    return  Math.round(((fahrenheit-32)*5)/9);
+}
